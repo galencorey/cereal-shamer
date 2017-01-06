cf9958f98c6f5c5b344b948e33fb10925db96827
Galen Corey <galen.corey@gmail.com>
2017-01-06 13:33:52 -0500

    added basic list of users

--- app/components/UsersList.jsx
+++ app/components/UsersList.jsx
@@ -4,13 +4,20 @@ import WhoAmI from './WhoAmI'
 export default class UsersList extends React.Component{
   constructor(props){
     super();
+
   }
 
   render(){
+    this.props.users = [{name: "Galen"}]
     return (
       <div>
         <WhoAmI />
         <p>Check out this cool list of users!</p>
+        <ul class="list-group">
+          {this.props.users && this.props.users.map(user=>{
+            return <li className="list-group-item">{user.name}</li>
+          })}
+        </ul>
       </div>
     )
   }
