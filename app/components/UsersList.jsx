94f14a7d22f9f54da47b40d09c2c8765fc898135
Galen Corey <galen.corey@gmail.com>
2017-01-06 12:46:45 -0500

    made userslist component

---
+++ app/components/UsersList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class UsersList extends React.Component{
+  constructor(props){
+
+  }
+
+  render(){
+    return <p>Check out this cool list of users!</p>
+  }
+}
